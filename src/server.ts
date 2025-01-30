4e4bd6198f84b3ea6e4c4960270c61bfa51b8c6c
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2025-01-30 22:34:27 +0100

    added cors

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,14 @@ import express from "express"
 import mongoose from "mongoose"
 import urlShorter from "./routes/UrlsRouter.js"
 import dotenv from "dotenv"
+import cors from "cors"
 
 dotenv.config()
 
 const app = express()
+
+app.use(cors());
+
 app.use(express.json())
 
 app.use("/api", urlShorter)
