b9943ef55343607f08df16cb753e79f18272fbb7
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2025-01-31 20:32:55 +0100

    end to end testing

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,29 @@
 import express from "express"
-import mongoose from "mongoose"
 import urlShorter from "./routes/UrlsRouter.js"
 import dotenv from "dotenv"
 import cors from "cors"
 import { connectDB } from "./db.js"
 
 dotenv.config()
-
 const app = express()
 
 app.use(cors())
-
 app.use(express.json())
-
 app.use("/api", urlShorter)
 
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 3001
+
 export const startServer = async () => {
+  try {
     await connectDB()
-    return app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`)
-    })
-}
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`)
+    });
+  } catch (error) {
+    console.error("Error starting the server:", error)
+  }
+};
+
+startServer()
 
 export default app
